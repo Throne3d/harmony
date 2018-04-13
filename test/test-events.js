3dec20b689acd55630007e1e62a3e15d44f956b2
Edward Jones <throne3d@gmail.com>
2018-04-13 02:03:37 +0100

    Test bindEvents

--- test/test-events.js
+++ test/test-events.js
@@ -1,6 +1,8 @@
 const harmony = require('../lib/harmony');
-const chai = require('chai');
+const winston = require('winston');
+const EventEmitter = require('events');
 const sinon = require('sinon');
+const chai = require('chai');
 const expect = chai.expect;
 
 describe('Harmony', function() {
@@ -15,6 +17,41 @@ describe('Harmony', function() {
       const bot = new harmony.Harmony();
       expect(bot.bindEvents.callCount).to.equal(1);
       harmony.Harmony.prototype.bindEvents.restore();
+    });
+  });
+
+  describe('bindEvents', function() {
+    beforeEach(function() {
+      this.bot = new harmony.Harmony('');
+      this.bot.client = new EventEmitter();
+      this.bot.bindEvents();
+    });
+
+    it('binds to ready', function() {
+      sinon.spy(winston, 'info');
+      this.bot.client.emit('ready');
+      expect(winston.info.args).to.deep.equal([
+        ['Logged in!']
+      ]);
+      winston.info.restore();
+    });
+
+    const list = {
+      message: 'processMessage',
+      error: 'handleError',
+      warn: 'handleWarning',
+    };
+
+    Object.keys(list).forEach(key => {
+      const value = list[key];
+      it(`binds to ${key}`, function() {
+        const stub = sinon.stub(this.bot, value);
+        this.bot.client.emit(key, 'arg example');
+        expect(stub.args).to.deep.equal([
+          ['arg example']
+        ]);
+        stub.restore();
+      });
     });
   });
 });
