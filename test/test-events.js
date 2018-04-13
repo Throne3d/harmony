993192679ebc86e6f1f2bee7526e1a76a1ca531c
Edward Jones <throne3d@gmail.com>
2018-04-13 01:29:10 +0100

    Test the constructor binds events

--- test/test-events.js
+++ test/test-events.js
@@ -1,5 +1,6 @@
 const harmony = require('../lib/harmony');
 const chai = require('chai');
+const sinon = require('sinon');
 const expect = chai.expect;
 
 describe('Harmony', function() {
@@ -7,6 +8,13 @@ describe('Harmony', function() {
     it('accepts a token', function() {
       const bot = new harmony.Harmony("token");
       expect(bot.token).to.equal("token");
+    });
+
+    it('binds events', function() {
+      sinon.spy(harmony.Harmony.prototype, 'bindEvents');
+      const bot = new harmony.Harmony();
+      expect(bot.bindEvents.callCount).to.equal(1);
+      harmony.Harmony.prototype.bindEvents.restore();
     });
   });
 });
